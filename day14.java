c198aa28b3730e019c877834b3c9291a78682219
rhit-bakerjl1 <bakerjl1@rose-hulman.edu>
2024-12-17 22:50:59 -0700

    Finished day 14

--- day14.java
+++ day14.java
@@ -47,8 +47,45 @@ public class day14 {
         int numCols = 101;
         // numRows = 7;
         // numCols = 11;
-        int numSecs = 100;
+        // int numSecs = 100;
 
+        for (int i = 1; i < 30000; i++) {
+            long myLine = safetyFactor(robotPosVel, numRows, numCols, i, false);
+            if (myLine > 300) {
+                System.out.println("Time = " + i + " s, Line Score = " + myLine);
+            }
+        }
+
+        long myLine = safetyFactor(robotPosVel, numRows, numCols, 17440, true);
+
+        // long fos = safetyFactor(robotPosVel, numRows, numCols, numSecs, true);
+
+        // System.out.println("Safety factor: " + fos);
+
+        // int halfway = (numCols-1)/2;
+        // int botCount = 0;
+        // for (int i = 0; i < numRows; i++) {
+        //     for (int j = 0; j < numCols; j++) {
+        //         if (j == halfway + i || j == halfway - i || i == halfway || j == halfway) {
+        //             System.out.print('1');
+        //             botCount++;
+        //             robotGrid[i][j] = 1;
+        //         } else {
+        //             System.out.print('.');
+        //             robotGrid[i][j] = 0;
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println("There are " + botCount + " bots in the tree");
+
+        // int myLine = lineScore(robotGrid);
+        // System.out.println("RobotGrid lineScore: " + myLine);
+
+    }   
+
+    public static long safetyFactor(ArrayList<int[]> robotPosVel, int numRows, int numCols, int numSecs, boolean display) {
         int[][] robotGrid = new int[numRows][numCols];
         for (int i = 0; i < robotGrid.length; i++) {
             for (int j = 0; j < robotGrid[1].length; j++) {
@@ -64,14 +101,16 @@ public class day14 {
         int[] qsData = {0,0,0,0};
         int midCol = (numCols-1)/2;
         int midRow = (numRows-1)/2;
-        int numBots = 0;
         for (int i = 0; i < robotGrid.length; i++) {
             for (int j = 0; j < robotGrid[0].length; j++) {
                 if (robotGrid[i][j] != 0) {
-                    System.out.print(robotGrid[i][j]);
-                    numBots += robotGrid[i][j];
+                    if (display) {
+                        System.out.print(robotGrid[i][j]);
+                    }
                 } else {
-                    System.out.print(".");
+                    if (display) {
+                        System.out.print(".");
+                    }
                 }
                 if (i < midRow) {
                     // Q1 or Q2
@@ -89,7 +128,9 @@ public class day14 {
                     }
                 }
             }
-            System.out.println();
+            if (display) {
+                System.out.println();
+            }
         }
 
         long fos = 1;
@@ -97,10 +138,9 @@ public class day14 {
             fos*= qsData[i];
         }
 
-        System.out.println("Num bots: " + numBots);
-        System.out.println("Safety factor: " + fos);
-
-    }   
+        // return fos;
+        return lineScore(robotGrid);
+    }
 
     public static int[] getLastPos(int[] posVelData, int numRows, int numCols, int numSecs) {
         int posX = posVelData[0];
@@ -116,5 +156,94 @@ public class day14 {
             lastPos[1] += numCols;
         }
         return lastPos;
+    }
+
+    public static int lineScore(int[][] robotGrid) {
+        int numRows = robotGrid.length;
+        int numCols = robotGrid[0].length;
+
+        // Horizontal Count
+        int horizCount = 0;
+        for (int i = 0; i < numRows; i++) {
+            horizCount += consecScore(robotGrid[i]);
+        }
+
+        // Vertical Count
+        int vertCount = 0;
+        int[] myCol = new int[numRows];
+        for (int i = 0; i < numCols; i++) {
+            for (int j = 0; j < numRows; j++) {
+                myCol[j] = robotGrid[j][i];
+            }
+            vertCount += consecScore(myCol);
+        }
+
+        // Diagonal Count
+        int upLeftCount = 0;
+        for (int i = numCols-1; i >= 0; i--) {
+            int[] myDiag = new int[numCols - i];
+            for (int j = 0; j < myDiag.length; j++) {
+                myDiag[j] = robotGrid[j][i+j];
+            }
+            upLeftCount += consecScore(myDiag);
+        }
+        for (int i = 1; i < numRows - numCols; i++) {
+            int[] myDiag = new int[numCols];
+            for (int j = 0; j < myDiag.length; j++) {
+                myDiag[j] = robotGrid[i+j][j];
+            }
+            upLeftCount += consecScore(myDiag);
+        }
+        for (int i = numRows - numCols; i < numRows; i++) {
+            int[] myDiag = new int[numRows - i];
+            for (int j = 0; j < myDiag.length; j++) {
+                myDiag[j] = robotGrid[i+j][j];
+            }
+            upLeftCount += consecScore(myDiag);
+        }
+
+        // Diagonal other count
+        int upRightCount = 0;
+        for (int i = 0; i < numCols; i++) {
+            int[] myDiag = new int[i];
+            for (int j = 0; j < myDiag.length; j++) {
+                myDiag[j] = robotGrid[j][i-j];
+            }
+            upRightCount += consecScore(myDiag);
+        }
+        for (int i = 1; i < numRows - numCols; i++) {
+            int[] myDiag = new int[numCols];
+            for (int j = myDiag.length-1; j >= 0; j--) {
+                myDiag[j] = robotGrid[numCols - j + i][j];
+            }
+            upRightCount += consecScore(myDiag);
+        }
+        for (int i = numRows - numCols; i < numRows; i++) {
+            int[] myDiag = new int[numRows - i];
+            for (int j = 0; j < myDiag.length; j++) {
+                myDiag[j] = robotGrid[i+j][numCols-j-1];
+            }
+            upRightCount += consecScore(myDiag);
+        }
+
+        return horizCount + vertCount + upLeftCount + upRightCount;
+    }
+
+    public static int consecScore(int[] nums) {
+        int score = 0;
+        int consec = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                consec += nums[i];
+            } else {
+                if (consec > 3) {
+                    score += consec;
+                }
+                consec = 0;
+            }
+        }
+        score += consec;
+
+        return score;
     }
 }
